e5239682e6b61852019d5aa0dbb0733763c92d34
Divanshu Kachhawa <divanshukachawa7878@gmail.com>
2024-02-08 12:00:34 +0530

    Loading Update

--- src/Components/loader/loader.jsx
+++ src/Components/loader/loader.jsx
@@ -1,6 +1,8 @@
 function Loader() {
     return(
-        <div className="h-[50px] w-[50px] rounded-full border-8 border-gray-400 border-t-[8px] border-t-green-800 animate-spin">
+        <div className="h-[50px] w-[50px] rounded-full border-8 border-gray-400 border-t-[8px] border-t-green-800 
+        border-b-[8px] border-b-green-800 
+        animate-spin">
 
         </div>
     )
